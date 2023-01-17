86d17e81863b11a351ceeeb40c939c658e18ff05
Eyal Yovel <eyalyovel@gmail.com>
2023-01-17 15:02:48 +0000

    Add SC example

---
+++ tests/specs/localDemoWebApp/login.local.js
@@ -0,0 +1,24 @@
+ import {Selector  as $} from 'testcafe';
+
+const inventoryList = $(".inventory_list");
+const usernameInput = $('#user-name');
+const passwordInput = $('#password');
+const loginBtn = $('#login-button');
+
+fixture ("Test login page")
+    .page("http://localhost:3000")
+    
+    test("should be able load the login page", async t => {
+        await t.expect(usernameInput.exists).ok('Page should be inventory',{ timeout: 500 });
+    } );
+    
+    test("should be able to login with a standard user", async t => {
+        await t
+            .typeText(usernameInput, "standard_user")
+            .typeText(passwordInput, "secret_sauce")
+            .click(loginBtn)
+
+            // Verification - check element exists
+            .expect(inventoryList.exists).ok('Page should be inventory',{ timeout: 500 });
+    } );
+ 
